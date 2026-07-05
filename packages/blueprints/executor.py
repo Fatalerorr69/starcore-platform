6357d30c1070801e43449444932e5f8563ed8612
Jacob Starko <105564503+Fatalerorr69@users.noreply.github.com>
2026-07-05 12:25:21 +0200

    Add BlueprintExecutor class with execute method

---
+++ packages/blueprints/executor.py
@@ -0,0 +1,25 @@
+"""
+Blueprint Executor
+"""
+
+from provider_sdk.registry import registry
+
+from .planner import ExecutionPlanner
+
+
+class BlueprintExecutor:
+
+    async def execute(self, blueprint):
+
+        planner = ExecutionPlanner()
+
+        plan = planner.create_plan(blueprint)
+
+        for step in plan:
+
+            provider = registry.get(step["provider"])
+
+            await provider.connect()
+
+            # Další implementace:
+            # await provider.create(step)
