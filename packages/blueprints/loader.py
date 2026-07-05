f8954e7043cead01cb2d664e3a96f1d2749676bb
Jacob Starko <105564503+Fatalerorr69@users.noreply.github.com>
2026-07-05 12:22:16 +0200

    Add BlueprintLoader class for loading blueprints

---
+++ packages/blueprints/loader.py
@@ -0,0 +1,20 @@
+"""
+Blueprint Loader
+"""
+
+from pathlib import Path
+
+import yaml
+
+from .models import Blueprint
+
+
+class BlueprintLoader:
+
+    @staticmethod
+    def load(path: str | Path) -> Blueprint:
+
+        with open(path, encoding="utf-8") as f:
+            data = yaml.safe_load(f)
+
+        return Blueprint.model_validate(data)
