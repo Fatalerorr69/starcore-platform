20deed1ad23512b800bba2579ababceda33d00f6
Jacob Starko <105564503+Fatalerorr69@users.noreply.github.com>
2026-07-05 10:02:02 +0200

    Implement ProviderManager class for managing providers

---
+++ packages/core/provider_manager.py
@@ -0,0 +1,21 @@
+"""
+Infrastructure Providers
+"""
+
+
+class ProviderManager:
+
+    def __init__(self):
+        self.providers = {}
+
+    def register(self, provider):
+
+        self.providers[provider.name] = provider
+
+    def get(self, name):
+
+        return self.providers.get(name)
+
+    def available(self):
+
+        return list(self.providers.keys())
