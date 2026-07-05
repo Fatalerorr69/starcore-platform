53f654f3dea34c9b2d4a483d6f1c6ceb457ea755
Jacob Starko <105564503+Fatalerorr69@users.noreply.github.com>
2026-07-05 10:00:18 +0200

    Implement simple EventBus class in events.py

---
+++ packages/core/events.py
@@ -0,0 +1,23 @@
+"""
+Simple Event Bus
+"""
+
+from collections import defaultdict
+from typing import Callable
+
+
+class EventBus:
+
+    def __init__(self):
+        self._events = defaultdict(list)
+
+    def subscribe(self, event: str, callback: Callable):
+        self._events[event].append(callback)
+
+    def emit(self, event: str, payload=None):
+
+        for callback in self._events[event]:
+            callback(payload)
+
+
+event_bus = EventBus()
